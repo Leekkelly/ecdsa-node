d62d44b2f81464467a8e9f18ced0d119246f8ebb
Leekkelly <kellylee.au@gmail.com>
2023-08-08 13:28:54 +1000

    1

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,27 +1,50 @@
 import server from "./server";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
-    }
-  }
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
 
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
+  async function onChange(evt) {
+    try{  
+      const privateKey = evt.target.value;
+      setPrivateKey(privateKey);
+      const public_key = secp.getPublicKey(privateKey);
+      const address = toHex(keccak256(public_key.slice(1)).slice(-20));
+      setAddress(address);
+      if (address) {
+        const {
+          data: { balance },
+        } = await server.get(`balance/${address}`);
+        setBalance(balance);
+      } else {
+        setBalance(0);
+      }
+    } catch (error) {
+      console.log("error!");
+      if (error) {
+        const privateKey = evt.target.value;
+        let address;
+        if( !privateKey) {
+          address = 'Enter a Private Key above';
+        } else {
+          address = 'Invalid Private Key'
+        }
+      }
+    }
+  }  
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type in a private key" value={privateKey} onChange={onChange}></input>
       </label>
+
+      <div>
+        Address: {address.slice(0, 10)}...
+      </div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ const app = express();
 const cors = require("cors");
 const port = 3042;
 
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "03c523b39a24d21c3e0158fc923df9c71db1ac0b7291ccecf8ec22cd451b7a72ce": 100, //dan public key
+  "020c7b96f18c43c94dfce11fb9a452591d5f18769db6ba376186536aba8f82a387": 50, //al
+  "026389b4edefdc0cfac206c5f2cca93554f74dbeb5d9146a3a4827b29852bdd052": 75, //john
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -18,15 +22,22 @@ app.get("/balance/:address", (req, res) => {
   res.send({ balance });
 });
 
+//to do: sign tx to server and recover the publickey
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, hash, recoveryBit } = req.body;
+
+  const message = {sender, amount, recipient};
+  const recovered_address = await secp.recoverPublicKey(keccak256(utf8ToBytes(JSON.stringify(message))), hash, recoveryBit);
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances[sender] < amount) {
+  if(toHex(keccak256(recovered_address.slice(1)).slice(-20)) !== sender) {
+    console.log("sender not matched");
+    res.status(400).send({ message: "sender not matched!"});
+  }else if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
-  } else {
+  }else {
     balances[sender] -= amount;
     balances[recipient] += amount;
     res.send({ balance: balances[sender] });
